81bf0908084b6d96e27dfd7acf7aba1c7ab75875
Wanderson Jordao <franciscowanderson26@gmail.com>
2025-05-05 14:43:40 -0300

    adicionando novas moedas

--- src/Principal.java
+++ src/Principal.java
@@ -6,27 +6,36 @@ public class Principal {
         Scanner entrada = new Scanner(System.in);
 
         String[][] opcoes = {
-                {"USD", "BRL"},
-                {"EUR", "USD"},
-                {"GBP", "BRL"},
-                {"BRL", "EUR"},
-                {"USD", "JPY"},
-                {"EUR", "CHF"}
+                {"USD", "BRL"}, // 1
+                {"EUR", "USD"}, // 2
+                {"GBP", "BRL"}, // 3
+                {"BRL", "EUR"}, // 4
+                {"USD", "JPY"}, // 5
+                {"EUR", "CHF"}, // 6
+                {"CAD", "BRL"}, // 7
+                {"BRL", "ARS"}, // 8
+                {"USD", "BTC"}, // 9
+                {"EUR", "JPY"}  // 10
         };
+
 
         HistoricoConversao historico = new HistoricoConversao();
         ServicoDeCambio servico = new ServicoDeCambio("7546a460f71243ac25acc562");
         ConversorMoeda conversor = new ConversorMoeda(servico);
 
         while (true) {
-            System.out.println("\n=== Conversor de Moedas ===");
+            System.out.println("=== Conversor de Moedas ===");
             System.out.println("1 - Dólar (USD) → Real (BRL)");
             System.out.println("2 - Euro (EUR) → Dólar (USD)");
             System.out.println("3 - Libra (GBP) → Real (BRL)");
             System.out.println("4 - Real (BRL) → Euro (EUR)");
             System.out.println("5 - Dólar (USD) → Iene (JPY)");
             System.out.println("6 - Euro (EUR) → Franco Suíço (CHF)");
-            System.out.println("7 - Ver histórico de conversões");
+            System.out.println("7 - Dólar Canadense (CAD) → Real (BRL)");
+            System.out.println("8 - Real (BRL) → Peso Argentino (ARS)");
+            System.out.println("9 - Dólar (USD) → Bitcoin (BTC)");
+            System.out.println("10 - Euro (EUR) → Iene (JPY)");
+            System.out.println("11 - Ver histórico de conversões");
             System.out.println("0 - Sair");
             System.out.print("Digite o número da opção: ");
             int opcao = entrada.nextInt();
@@ -36,12 +45,12 @@ public class Principal {
                 break;
             }
 
-            if (opcao == 7) {
+            if (opcao == 11) {
                 historico.exibir();
                 continue;
             }
 
-            if (opcao < 1 || opcao > 6) {
+            if (opcao < 1 || opcao > 10) {
                 System.out.println("Opção inválida.");
                 continue;
             }
